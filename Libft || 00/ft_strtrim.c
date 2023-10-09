90bc3dd1a275255de4b7f1f6cda4d593dedbf9ab
Noel <128312469+Z1nnlos@users.noreply.github.com>
2023-10-09 09:15:09 +0200

    1

--- Libft || 00/ft_strtrim.c
+++ Libft || 00/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: noel <noel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 13:50:10 by noel              #+#    #+#             */
-/*   Updated: 2023/10/08 19:23:46 by noel             ###   ########.fr       */
+/*   Updated: 2023/10/09 09:12:49 by noel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,26 @@ char *ft_strtrim(char const *s1, char const *set)
 {
     char *result;
     int i1 = 0;
-    int i2 = ft_strlen(s1) -1;
-
-    while (s1[i] != 0 && ft_str)
+    int s1size = ft_strlen(s1) -1;
+    int setsize = ft_strlen(set) - 1;
+    int setcounter = 0;
+    
+    //Herausfinden, ob set vorkommt und wenn ja vorne und hinten?
+    while (s1[i1] != 0)
+    {
+        if (s1[i1] == set[0] && ft_strncmp(s1, set, setsize) == 0)
+        {
+            setcounter++;
+        }
+        i1++;
+    }
+    printf("%d", setcounter);
+    
 }
 
 int main()
 {
     const char *s1 = "abc Hello there abc";
     const char *set = "abc";
-    printf("%s", ft_strtrim(s1, set));
+    ft_strtrim(s1, set);
 }

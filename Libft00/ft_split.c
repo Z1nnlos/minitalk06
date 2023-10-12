a32ddb0437534923dc65a9869d08550ee6147126
Z1nnlos <Noel.Sabia@gmx.net>
2023-10-12 15:02:16 +0200

    safety push

--- Libft00/ft_split.c
+++ Libft00/ft_split.c
@@ -6,25 +6,49 @@
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 17:35:21 by nsabia            #+#    #+#             */
-/*   Updated: 2023/10/12 14:37:00 by nsabia           ###   ########.fr       */
+/*   Updated: 2023/10/12 15:00:54 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+void new_strring(const char *s, char c, char **result, int stri)
+{
+	int	i;
+	int	chari;
+
+	chari = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			result[stri][chari] = s[i];
+			i++;
+			chari++;
+		}
+		if (s[i] == c || s[i] == '\0')
+		{
+			result[stri][chari] = '\0';
+			stri++;
+			chari = 0;
+			while (s[i] == c)
+				i++;
+		}
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
 	int		i;
 	int		stri;
-	int		chari;
 	int		ssize;
 	int		sizeofal;
 
 	i = 0;
 	stri = 0;
-	chari = 0;
 	ssize = ft_strlen(s);
 	while (s[i] != '\0')
 	{
@@ -45,24 +69,6 @@ char	**ft_split(char const *s, char c)
 			return (NULL);
 		result[i][0] = '\0';
 		i++;
-	}
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] != c)
-		{
-			result[stri][chari] = s[i];
-			i++;
-			chari++;
-		}
-		if (s[i] == c || s[i] == '\0')
-		{
-			result[stri][chari] = '\0';
-			stri++;
-			chari = 0;
-			while (s[i] == c)
-				i++;
-		}
 	}
 	result[stri] = NULL;
 	return (result);

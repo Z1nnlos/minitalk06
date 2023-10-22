b0889feddf96da16e33fbe5cb4eec2f94e97a160
Z1nnlos <Noel.Sabia@gmx.net>
2023-10-22 18:50:11 +0200

    nearly finished

--- Libft00/ft_split.c
+++ Libft00/ft_split.c
@@ -6,37 +6,70 @@
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 17:35:21 by nsabia            #+#    #+#             */
-/*   Updated: 2023/10/22 14:29:25 by nsabia           ###   ########.fr       */
+/*   Updated: 2023/10/22 18:42:32 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// int	wordcounting(const char *s, char c)
-// {
-// 	int		i;
-// 	int		o;
+#include <stdlib.h>
+#include "libft.h"
 
-// 	i = 0;
-// 	o = 0;
-// 	while (s)
-// 	{
-// 		if (s[i] != c)
-// 			i++;
-// 		else
-// 			o++;
-// 		s++;
-// 	}
-// 	if (s[i - 1] != c)
-// 		i += 1;
-// 	return (i);
-// }
+int	wordcounting(const char *s, char c)
+{
+	int	i;
+	int	o;
 
-// char	**ft_split(const char *s, char c)
-// {
-// 	int		word_counter;
-// 	char	**result;
-// 	int		word_length;
-// 	int		i;
+	i = 0;
+	o = 0;
+	while (*s != 0)
+	{
+		if (*s != c)
+			i++;
+		else if (*s == c)
+			o++;
+		s++;
+	}
+	return (o);
+}
 
-// 	word_counter = wordcounting(s, c);
-// }
+int	malloc_check(char **result, int i)
+{
+	if (!result[i])
+	{
+		while (i--)
+		{
+			free (result[i]);
+		}
+		free (result);
+		return (1);
+	}
+	return (0);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	int		word_counter;
+	char	**result;
+	int		word_length;
+	int		i;
+
+	word_counter = wordcounting(s, c);
+	result = (char **)ft_calloc(word_counter + 1, sizeof(char *));
+	if (!result)
+		return (NULL);
+	i = 0;
+	word_length = 0;
+	while (i < word_counter)
+	{
+		if (*s != 0 && *s == c)
+			s++;
+		while (s[word_length] && s[word_length] != c)
+			word_length++;
+		result[i] = ft_substr(s, 0, word_length);
+		i++;
+		if (malloc_check(result, i) == 1)
+			return (NULL);
+		s += word_length;
+	}
+	return (result);
+}
 //static nicht vergessen
